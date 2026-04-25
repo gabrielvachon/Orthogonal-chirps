2099870f265144d9a7fa0e01ff8409f724535ee4
Gabriel Vachon <gabrielvachon3@gmail.com>
2026-04-25 19:14:00 -0400

    added optimal chirp selection by autocorrelation and cross-correlation analysis

--- demo_orthogonal_chirp.py
+++ demo_orthogonal_chirp.py
@@ -3,12 +3,14 @@ from scipy import signal
 import numpy as np
 import orthogonal_chirp
 
-M = 4
-T = 2
-f0 = 4000
-f1 = 10000
+M = 8
+T = 1
+f0 = 5500
+f1 = 9500
 fs = 48000
-chirp = orthogonal_chirp.orthogonal_chirp_base(fs, f0, f1, M, T, 'hybrid')
+chirp = orthogonal_chirp.orthogonal_chirp_base(fs=fs, f0=f0, f1=f1, M=M, T=T, type='hybrid', method='hyper', window='kaiser', win_b=2, optimal=True, max_candidate=100)
+
+chirp.write(path="./")
 
 plt.figure()
 Sxx_tot = 0
@@ -17,15 +19,14 @@ for m in chirp.matrix:
    Sxx_tot = Sxx_tot + Sxx
 plt.ylim([f0, f1])
 plt.pcolormesh(s, f, np.abs(Sxx_tot))
+plt.figure()
 
-
-for m in range(M):
-   fig, ax = plt.subplots(M)
-   fig.suptitle(f"Unity height pulse {m}")
-   for n in range(M):
-      ax[M-1-n].plot(chirp.uhb[m][n])
-      ax[M-1-n].set_ylabel(f"Band {n}")
-
+Sxx_tot = 0
+for m in chirp.bb_up:
+   f, s, Sxx = signal.stft(x=m, fs=fs, nperseg=1024, noverlap=512)
+   Sxx_tot = Sxx_tot + Sxx
+plt.ylim([f0, f1])
+plt.pcolormesh(s, f, np.abs(Sxx_tot))
 
 fig, ax = plt.subplots(M) 
 for m in range(M):
@@ -35,5 +36,25 @@ for m in range(M):
    ax[m].pcolormesh(s, f, np.abs(Sxx))
       
 plt.show()
+
+time = np.arange(start=-len(chirp[0])/fs, stop=(len(chirp[0])-1)/fs, step=1/fs)
+m_idx = 0
+n_idx = 0
+for m in chirp:
+    n_idx = 0
+    fig, ax = plt.subplots(M)
+    for n in chirp:
+        corr = signal.correlate(in1=m, in2=n, method='fft', mode='full') / (np.sqrt(np.sum(m ** 2)) * np.sqrt(np.sum(n ** 2)))
+        
+        if m_idx == n_idx:
+            ax[n_idx].set_title(f"Autocorrelation for chirp {n_idx}")
+            ax[n_idx].plot(time, corr, "C3")
+        else:
+            ax[n_idx].set_title(f"Cross-correlation with chirp {n_idx}")
+            ax[n_idx].plot(time, corr, "C0")
+        ax[n_idx].set_ylim(-1.1, 1.1)
+        n_idx += 1
+    plt.show()
+    m_idx += 1
 
 print(chirp)
